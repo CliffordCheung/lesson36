9226da40750f706771c8e38113a1026a793b5247
CliffordCheung <cliffordcheung77@gmail.com>
2025-04-16 21:57:29 +0800

    new commit with yaml in the correct folder test 10

--- backend.tf
+++ backend.tf
@@ -2,9 +2,8 @@
 
 terraform {
   backend "s3" {
-    bucket  = "sctp-ce9-tfstate"
-    key     = "clifford-lesson36.tfstate" #Change the value of this to <your suggested name>.tfstate for  example
-    region  = "us-east-1"
-    version = ">= 1.0"
+    bucket = "sctp-ce9-tfstate"
+    key    = "clifford-lesson36.tfstate" #Change the value of this to <your suggested name>.tfstate for  example
+    region = "us-east-1"
   }
 }
